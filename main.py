ac6a1e9b3ca3ac1f98aaacaf28759516c4b90a3d
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-07-30 15:10:50 -0400

    Added support for adding to github quote repo

--- main.py
+++ main.py
@@ -49,16 +49,37 @@ def receive_message(bot_number, weather_api_key):
             current_message_frame_lines = []
         current_message_frame_lines.append(line)
 
+    # Loop through each message and check if it is a command
     for message_frame in message_frames: 
         if message_frame.msg_type == "received_message":
-            if message_frame.message == "#currenttime":
+            if message_frame.message == "!currenttime":
                 current_time = datetime.datetime.now().strftime("%H:%M:%S")
                 send_message(bot_number, f"{current_time}", message_frame.sender_number, message_frame.group_id)
         
-            elif message_frame.message == "#currentweather": 
-                wheather_response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q=Gothenburg")
-                current_temp = wheather_response.json()['current']['temp_c']
-                send_message(bot_number, f"Current temp in Gothenburg: {current_temp}", message_frame.sender_number,message_frame.group_id)
+            elif message_frame.message[0:15] == "!currentweather": 
+                if message_frame.message == "!currentweather": # Sets a default location if no location is specified
+                    message_frame.message = "!currentweather Gothenburg"
+
+                location = message_frame.message.split(" ")[1]
+                weather_response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q={location}")
+                if weather_response.status_code == 200:
+                    current_temp = weather_response.json()['current']['temp_c']
+                    send_message(bot_number, f"Current temp in {location}: {current_temp}", message_frame.sender_number,message_frame.group_id)
+
+                else:
+                    send_message(bot_number, f"Sorry I can't find the weather for {location}", message_frame.sender_number, message_frame.group_id)
+
+            elif message_frame.message[0:7] == "!quote ": 
+                new_quote = message_frame.message.split("!quote ")[1]
+                subprocess.run(f"git -C quotes/ pull", shell=True,capture_output=True)
+                subprocess.run(f"echo ' ' >> quotes/quotes.tex", shell=True,capture_output=True)
+                subprocess.run(f"echo {new_quote} >> quotes/quotes.tex", shell=True,capture_output=True)
+                subprocess.run(f"git -C quotes/ add .", shell=True,capture_output=True)
+                subprocess.run(f"git -C quotes/ commit -m 'New quote added from bot'", shell=True,capture_output=True)
+                subprocess.run(f"git -C quotes/ push", shell=True,capture_output=True)
+                send_message(bot_number, f"Quote added!", message_frame.sender_number, message_frame.group_id)
+
+
 
 def main():
     print("Bot started!")
